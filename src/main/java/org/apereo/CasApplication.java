61cad70ba17e64fb00b3ccb3eaefbb34bf8ac71a
cas-initializr <info@apereo.org>
2022-12-30 21:09:23 +0000

    Project created by Apereo CAS Initializr

---
+++ src/main/java/org/apereo/CasApplication.java
@@ -0,0 +1,13 @@
+package org.apereo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasApplication.class, args);
+	}
+
+}
